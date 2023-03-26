df4616094a65cdf51dc1f8e6cceadd2ad53dc7b2
Mohak Tandon <er.mohak97@gmail.com>
2023-03-26 19:35:31 +0530

    lastchange

--- my-captcha/src/components/SignUpForm.jsx
+++ my-captcha/src/components/SignUpForm.jsx
@@ -5,6 +5,9 @@ import "./Signup.css"
 function SignUpForm() {
     const [cap,setcap]=useState("")
     const [checkcap,setcapcheck]=useState("")
+    const [name,setname]=useState("")
+    const [email,setemail]=useState("")
+    const [password,setpassword]=useState("")
     
 
 
@@ -23,24 +26,38 @@ function SignUpForm() {
     useEffect(()=>{
         makecaptcha(5)
     },[])
-    const handlesubmit=()=>{
+    const handlesubmit=(e)=>{
+       
+       
+    
+        if (!name || !email || !password) {
+        
+          alert('Please fill in all required fields.');
+          return;
+        }
         if(cap===checkcap){
             alert("Signup Successfull")
             setcapcheck("")
+            setemail("")
+            setpassword("")
+            setname("")
         }else{
             alert("Invalid captcha")
             setcapcheck("")
+            setemail("")
+            setpassword("")
+            setname("")
         }
     }
   return (
     <div style={{marginTop:'100px'}}>
     <div style={{width:'30%',margin:"auto"}}> 
-    <input placeholder="Name" style={{display: 'block', marginBottom: '10px',width:'200px'}} />
-        <input placeholder="Email" style={{display: 'block', marginBottom: '10px',width:'200px'}}/>
-        <input placeholder="Password" style={{display: 'block', marginBottom: '10px',width:'200px'}}/>
+    <input placeholder="Name" value={name} onChange={(e)=>setname(e.target.value)} style={{display: 'block', marginBottom: '10px',width:'200px'}} />
+        <input required placeholder="Email" value={email} onChange={(e)=>setemail(e.target.value)} style={{display: 'block', marginBottom: '10px',width:'200px'}}/>
+        <input value={password} placeholder="Password"  onChange={(e)=>setpassword(e.target.value)} style={{display: 'block', marginBottom: '10px',width:'200px'}}/>
         <button style={{display:"block"}}>{cap}</button>
         <input style={{marginRight:'115px',width:'200px'}} type="text" placeholder="Entercaptcha" value={checkcap} onChange={(e)=>setcapcheck(e.target.value)}/>
-        <button onClick={handlesubmit} className='button'>Submit</button>
+        <button type="submit" onClick={handlesubmit} className='button'>Submit</button>
        
     </div>
     </div>
